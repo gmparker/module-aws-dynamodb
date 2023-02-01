c7fd41426e2bd665b821857e2c5fc3f14264fe9f
George Parker <george.parker@lytx.com>
2023-02-01 09:03:34 -0800

    initial repository

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
-#
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_dynamodb_table" "http-crud-tutorial-items" {
+  name             = "http-crud-tutorial-items"
+  hash_key         = "id"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  replica {
+    region_name = "us-east-2"
+    propagate_tags = true
+  }
+
+  tags = {
+    Name        = "http-crud-tutorial-items"
+    team        = "enterprise architecture"
+  }
+}
